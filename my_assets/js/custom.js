bb7293a1ffb66c4698f4120404b41bfc053531bf
Oucif Abd Elkarim <oucifkarim01@gmail.com>
2021-05-18 12:47:30 +0100

    home page meniu

---
+++ my_assets/js/custom.js
@@ -0,0 +1,43 @@
+
+
+function mainMenuService(){
+    
+	$('.menu-carousel').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots: false,
+		smartSpeed: 500,
+		autoplayTimeout: 2000,
+		autoplay: false,
+		autoplayHoverPause:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			480:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			800:{
+				items:3
+			},
+			1200:{
+				items:3
+			}
+		}
+	});    		
+
+	let menu_holder = $('.menu-holder');
+	$('.menu-open_btn').click(function(){
+		menu_holder.removeClass('closed');
+	})
+	$('.menu-close_btn').click(function(){
+		menu_holder.addClass('closed');
+	})
+	setTimeout(function(){
+		menu_holder.addClass('animated');
+	},700)
+}
